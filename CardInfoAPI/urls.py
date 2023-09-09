29fb2f5c6f6fa00ef70a622cff09d47d0a9cb8d3
heihei <raions.seibu@gmail.com>
2023-09-09 20:04:00 +0900

    APIドキュメント作成

--- CardInfoAPI/urls.py
+++ CardInfoAPI/urls.py
@@ -15,11 +15,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 import sys
 sys.path.append('../')
 from cards.views import CreditCardListCreate
 
+# swaggerでAPIドキュメント作成
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Crefid Card API',
+        default_version='v1',
+        description='クレジットカード情報を取得するAPI',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cards/', include('cards.urls'))
+    path('cards/', include('cards.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
 ]
