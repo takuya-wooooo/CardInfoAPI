9b3f9209ce939709262f6ca6d56c547fc95a10da
heihei <raions.seibu@gmail.com>
2023-09-10 10:05:55 +0900

    レートリミットの設定追加

--- CardInfoAPI/settings.py
+++ CardInfoAPI/settings.py
@@ -85,6 +85,14 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/hour',
+        'user': '1000/hour'
+    },
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'EXCEPTION_HANDLER': 'rest_framework_json_api.exceptions.exception_handler',
     'PAGE_SIZE': 10,
