2a2349ace2deeb6e2f5cd0322ffc993590b92fa8
heihei <raions.seibu@gmail.com>
2023-09-11 15:06:05 +0900

    レビュー機能を追加

--- cards/admin.py
+++ cards/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import CreditCard, CardCategory
+from .models import CreditCard, CardCategory, CardReview
 
 admin.site.register(CreditCard)
 admin.site.register(CardCategory)
+admin.site.register(CardReview)
--- cards/urls.py
+++ cards/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CreditCardListCreate, CardCategoryListCreate
+from .views import CreditCardListCreate, CardCategoryListCreate, CardReviewListCreate
 
 urlpatterns = [
     path('', CreditCardListCreate.as_view()),
     path('category', CardCategoryListCreate.as_view()),
     path('categories/<int:category_id>/cards/', CardCategoryListCreate.as_view()),
+    path('review', CardReviewListCreate.as_view()),
 ]
