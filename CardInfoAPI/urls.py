2414c49b72995905cd7c4b9d45a8cc8470931db0
heihei <raions.seibu@gmail.com>
2023-09-11 16:19:24 +0900

    カード比較機能を追加

--- CardInfoAPI/urls.py
+++ CardInfoAPI/urls.py
@@ -32,7 +32,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cards/', include('cards.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
-    path('sentry-debug/', include('cards.urls')),
+    path('cards', include('cards.urls')),
+    path('swagger', schema_view.with_ui('swagger', cache_timeout=0)),
+    path('sentry-debug', include('cards.urls')),
 ]
